e9ca2555a006874feff16bb13e42f1cf0c13f93f
danielh358 <99032217+danielh358@users.noreply.github.com>
2022-02-17 11:57:17 -0500

    Create server-setup.sh

---
+++ server-setup.sh
@@ -0,0 +1,7 @@
+#User data for an AWS instance running a Linux AMI:
+yum -y install docker
+systemctl unmask docker
+systemctl start docker
+docker run --name aline-mysql -p 3306 -e MYSQL_ROOT_PASSWORD=definitely_a_real_password -d mysql:latest
+
+#Then connect to the server using the credentials set up and use something like MySQL Workbench to upload model.
