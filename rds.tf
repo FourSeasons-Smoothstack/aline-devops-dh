d3e9b0845eb4bf6e14c1fd608506a33c7b3e22c2
danielh358 <99032217+danielh358@users.noreply.github.com>
2022-05-09 14:07:50 -0400

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -1,9 +1,9 @@
-resource "aws_db_instance" "alinedbdh" {
+resource "aws_db_instance" "aline-db-dh" {
   allocated_storage    = 10
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
-  name                 = "aline-db-dh"
+  name                 = "alinedbdh"
   username             = "admin"
   password             = "$$$$change-me$$$$$"
   parameter_group_name = "default.mysql5.7"
