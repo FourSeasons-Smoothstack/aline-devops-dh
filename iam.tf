3c57d80c6ba9da05de482479841459bd7ca07428
danielh358 <99032217+danielh358@users.noreply.github.com>
2022-05-09 11:31:35 -0400

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -46,7 +46,7 @@ resource "aws_iam_role_policy_attachment" "task_s3" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "task_s3" {
+resource "aws_iam_role_policy_attachment" "task_s3-2" {
   role       = aws_iam_role.ecs_task_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
