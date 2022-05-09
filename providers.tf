6cb3db36a6318827351af918d68ff90bad716b61
danielh358 <99032217+danielh358@users.noreply.github.com>
2022-05-09 13:21:12 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,8 +1,15 @@
 #Provider Info
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.11.0"
+    }
+  }
+}
 
 provider "aws" {
-  profile = "default"
   region  = "us-east-1"
   shared_config_files      = ["$HOME/.aws/conf"]
   shared_credentials_files = ["$HOME/.aws/creds"]
