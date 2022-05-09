36e1f1424fd5abaac047a182b35f7531a3dd873a
danielh358 <99032217+danielh358@users.noreply.github.com>
2022-05-09 12:51:55 -0400

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,13 +1,5 @@
 #Provider Info
 
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "4.11.0"
-    }
-  }
-}
 
 provider "aws" {
   profile = "default"
