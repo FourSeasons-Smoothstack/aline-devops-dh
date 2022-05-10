ce95e5e002f6d37a682159fb2479f74b09f0d8be
danielh358 <99032217+danielh358@users.noreply.github.com>
2022-05-10 11:14:18 -0400

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -8,4 +8,6 @@ resource "aws_db_instance" "aline-db-dh" {
   password             = "$$$$change-me$$$$$"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
+  storage_encrypted =  true
+  backup_retention_period = 12
 }
